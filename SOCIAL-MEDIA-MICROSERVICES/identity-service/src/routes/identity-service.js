de65b8cdf7422ee07b95e40db5cf59cde863df89
“Anish508” <anishbarke9741@gmail.com>
2025-07-07 20:52:05 +0530

    auth services completed

--- SOCIAL-MEDIA-MICROSERVICES/identity-service/src/routes/identity-service.js
+++ SOCIAL-MEDIA-MICROSERVICES/identity-service/src/routes/identity-service.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const { registerUser } = require('../controllers/identity-controller.js')
+const { registerUser, loginUser,userRefreshToken,logoutUser } = require('../controllers/identity-controller.js')
 
 const router = express.Router()
 
 router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.post('/refreshToken', userRefreshToken)
+router.post('/logout', logoutUser)
+
 
 module.exports = router
--- SOCIAL-MEDIA-MICROSERVICES/identity-service/src/server.js
+++ SOCIAL-MEDIA-MICROSERVICES/identity-service/src/server.js
@@ -21,7 +21,12 @@ const redisClient = new Redis(process.env.REDIS_URL);
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use((req, res, next) => {
